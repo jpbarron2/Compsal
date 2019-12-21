175c5fc52de5c07da491a105f7dca8c455f1123b
jirish <jasonpbarron2@gmail.com>
2019-12-20 18:51:27 -0800

    Removed Debug Status

--- CompSal/settings.py
+++ CompSal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0_80$n$imupjub+^+7oh(a6+wf8xd%jdxkw_=g@#i*0g@9t0i2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
